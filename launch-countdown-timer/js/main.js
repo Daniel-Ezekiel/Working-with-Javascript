55e9b06eb3c9b4dbde70f2c1b1a4bfb33276bb25
Daniel Ezekiel <dbezekiel77@gmail.com>
2023-03-28 11:22:00 +0100

    feat: implemented date input initialisation and generic date initialisation

--- launch-countdown-timer/js/main.js
+++ launch-countdown-timer/js/main.js
@@ -7,7 +7,7 @@ const labelSeconds = document.querySelector('.timer__seconds');
 
 const arr = [labelDays, labelHours, labelMinutes, labelSeconds];
 
-let time = 150000;
+let time;
 
 const calcDays = _ => {
   return Math.floor(time / (1000 * 60 * 60 * 24));
@@ -29,33 +29,64 @@ const calcSeconds = _ => {
 };
 
 function showCountdown() {
-  const days = calcDays();
-  const hours = calcHours();
-  const minutes = calcMinutes();
-  const seconds = calcSeconds();
-
-  labelDays.textContent = days;
-  labelHours.textContent = hours;
-  labelMinutes.textContent = minutes;
-  labelSeconds.textContent = seconds;
-  //   console.log(days, hours, minutes, seconds);
+  labelDays.textContent = calcDays();
+  labelHours.textContent = calcHours();
+  labelMinutes.textContent = calcMinutes();
+  labelSeconds.textContent = calcSeconds();
 }
 
-const timerCount = setInterval(() => {
-  time -= 1000;
-  showCountdown();
+function beginCountdown() {
+  const timerCount = setInterval(() => {
+    time -= 1000;
+    showCountdown();
 
-  if (time === 0) {
-    clearInterval(timerCount);
-    console.log('Timer Done!');
-  }
-  //   console.log(time);
-}, 1000);
+    if (time === 0) {
+      clearInterval(timerCount);
+      console.log('Timer Done!');
+    }
+    //   console.log(time);
+  }, 1000);
+}
 
+// MODAL SETUP
 const btnCloseModal = document.querySelector('.btn--close');
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
-btnCloseModal.addEventListener('click', _ => {
+
+const closeModal = () => {
   modal.classList.add('disabled');
   overlay.classList.add('disabled');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+
+// DATE PICKER USING DATE INPUT FIELD
+const tomorrowInMilli = Number(new Date()) + 24 * 60 * 60 * 1000;
+const tomorrow = new Date(tomorrowInMilli).toISOString();
+console.log(tomorrow.slice(0, tomorrow.indexOf('T')));
+
+const datePicker = document.querySelector('input[type="date"]');
+datePicker.setAttribute('min', tomorrow.slice(0, tomorrow.indexOf('T')));
+
+modal.addEventListener('click', e => {
+  // if (e.target.classList.contains('btn--user-input')) {
+  // }
+
+  if (e.target.classList.contains('btn--date-input')) {
+    const dateNum =
+      Number(new Date(datePicker.value)) ||
+      Number(new Date(datePicker.getAttribute('min')));
+
+    time = dateNum - new Date();
+    closeModal();
+    showCountdown();
+    beginCountdown();
+  }
+
+  if (e.target.classList.contains('btn--generic-input')) {
+    time = 2592000000;
+    closeModal();
+    showCountdown();
+    beginCountdown();
+  }
 });
